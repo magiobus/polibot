877b324b0bae5d81b262e409a46de88233625bf2
Alberto Bustillos <aalbertobus@gmail.com>
2016-11-19 23:00:43 -0600

    Candidate start values updated
    Change this two lines:
    budget and tendency
    
    check comments
    
    |> Map.put(:budget, generate_random_number(6))  #between 45-50MILLIONS
    |> Map.put(:tendency, assign_random(:tendency))     #-30 to 30

--- web/services/candidate.ex
+++ web/services/candidate.ex
@@ -17,12 +17,12 @@ defmodule Polibot.CandidateServices do
          |> Map.put(:race, assign_random(:race))
          |> Map.put(:gender, assign_random(:gender))
          |> Map.put(:was_poor, generate_boolean_state)
-         |> Map.put(:budget, generate_random_number(6))
-         |> Map.put(:popularity, generate_random_percentage(40, 100))
-         |> Map.put(:actual_popularity, generate_random_percentage(25, 100))
+         |> Map.put(:budget, generate_random_number(6)) 
+         |> Map.put(:popularity, generate_random_percentage(15, 40))
+         |> Map.put(:actual_popularity, generate_random_percentage(25, 100)) 
          |> Map.put(:last_name, assign_random(:last_name))
-         |> Map.put(:tendency, assign_random(:tendency))
-         |> Map.put(:charisma, generate_random_percentage(30, 100))
+         |> Map.put(:tendency, assign_random(:tendency)) 
+         |> Map.put(:charisma, generate_random_percentage(15,30))
 
     Map.put(info, :first_name, assign_random(:first_name, info))
   end
