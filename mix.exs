73062fd224ce1b806d3425ef8023312654af1bf8
Zura Guerra <guerrazuramx@gmail.com>
2016-11-19 21:02:52 -0600

    Add extwitter

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Polibot.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:httpotion, "~> 3.0"}]
+     {:httpotion, "~> 3.0"},
+     {:extwitter, "~> 0.7.2"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- web/controllers/chat_controller.ex
+++ web/controllers/chat_controller.ex
@@ -1,6 +1,8 @@
 defmodule Polibot.ChatController do
   use Polibot.Web, :controller
 
+  @messages_url "https://graph.facebook.com/v2.6/me/messages?access_token=" <> System.gen_env("POLIBOT_FB_TOKEN")
+
   def fb_callback(conn, %{"object" => "page"}) do
     render conn, "fb_callback.json"
   end
