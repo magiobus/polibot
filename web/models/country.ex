9d439914c4636d696740feb7403186a5160c3fc2
Zura Guerra <guerrazuramx@gmail.com>
2016-11-19 18:10:20 -0600

    Gen country model

---
+++ web/models/country.ex
@@ -0,0 +1,18 @@
+defmodule Polibot.Country do
+  use Polibot.Web, :model
+
+  schema "countries" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+  end
+end
