f89a5d8379f15c00ef03af875ca0fe93e82f054c
Zura Guerra <guerrazuramx@gmail.com>
2016-11-19 21:17:55 -0600

    Respond to start message

--- web/controllers/chat_controller.ex
+++ web/controllers/chat_controller.ex
@@ -3,6 +3,12 @@ defmodule Polibot.ChatController do
 
   @messages_url "https://graph.facebook.com/v2.6/me/messages?access_token=" <> System.gen_env("POLIBOT_FB_TOKEN")
 
+  def chat(conn, %{"entry" => [%{"messaging" => [%{"postback" => %{"payload" => "Let's run for presidency!"},
+                                                   "recipient" => %{"id" => page_id},
+                                                   "sender" => %{"id" => user_id}}|_]}|_]}) do
+    render conn, "fb_callback.json"
+  end
+
   def fb_callback(conn, %{"object" => "page"}) do
     render conn, "fb_callback.json"
   end
